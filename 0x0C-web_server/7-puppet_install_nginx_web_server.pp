0806e37e1aa3ebfff0aa507cbf1068b06fef51b7
grayolaRSA <gtndanga@gmail.com>
2023-07-07 18:25:11 +0200

    update

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,6 +2,6 @@
 # installs nginx server | adds 300 and 400 error codes
 
 exec { 'install':
-  provider => 'shell',
+  provider => shell,
   command  => 'sudo apt-get -y update && sudo apt-get -y install nginx && echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html && sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\\/redirect_me return 301 https:\\/\\/www.freecodecamp.org\\/ permanent;/" /etc/nginx/sites-available/default && sudo service nginx start',
 }
