b2c174c3d988e9a56196bd0d11d69254b92afcee
grayolaRSA <gtndanga@gmail.com>
2023-08-12 19:36:40 +0200

    update

--- 0x16-api-advanced/0-subs.py
+++ 0x16-api-advanced/0-subs.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """api module to get number of reddit subscribers for a subreddit"""
 import praw
+import prawcore.exceptions
 import sys
 
 
 def number_of_subscribers(subreddit):
     """method to retrieve the number of reddit subcribers"""
+
+    if not subreddit:
+        return 0
 
     # Create a Reddit instance
     reddit = praw.Reddit(
@@ -13,19 +17,21 @@ def number_of_subscribers(subreddit):
         client_secret='89vvf_RLXwjzISEyUslqv_O2u8303w',
         user_agent='YOUR_USER_AGENT'
     )
-    subreddit = sys.argv[1]
     subreddit_name = subreddit
+    try:
+        # Get the subreddit object
+        subreddit = reddit.subreddit(subreddit_name)
 
-    # Get the subreddit object
-    subreddit = reddit.subreddit(subreddit_name)
+        # Get the number of subscribers
+        subscribers_count = subreddit.subscribers
 
-    # Get the number of subscribers
-    subscribers_count = subreddit.subscribers
+        return subscribers_count
 
-    return subscribers_count
+    except prawcore.exceptions.NotFound:
+        return 0
 
 
 if __name__ == "__main__":
     subreddit_name = sys.argv[1]
     subscribers = number_of_subscribers(subreddit_name)
-    print(f"The '{subreddit_name}' subreddit has {subscribers} subscribers.")
+#   print(f"The '{subreddit_name}' subreddit has {subscribers} subscribers.")
