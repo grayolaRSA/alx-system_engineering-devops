cd73de4c97daef1bf8aa2c2397997db22c49cffc
grayolaRSA <gtndanga@gmail.com>
2023-08-12 19:46:59 +0200

    update

--- 0x16-api-advanced/0-subs.py
+++ 0x16-api-advanced/0-subs.py
@@ -1,37 +1,26 @@
 #!/usr/bin/python3
-"""api module to get number of reddit subscribers for a subreddit"""
-import praw
-import prawcore.exceptions
+"""
+Module for the reddit API queries
+"""
+import requests
 import sys
 
 
 def number_of_subscribers(subreddit):
-    """method to retrieve the number of reddit subcribers"""
+    """ Reddit API method """
+    u_agent = 'Mozilla/5.0'
 
-    if not subreddit:
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    url = (f"https://www.reddit.com/r/{subreddit}/about.json")
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
         return 0
-
-    # Create a Reddit instance
-    reddit = praw.Reddit(
-        client_id='F3jxoROf0m2Phw3to2QuLA',
-        client_secret='89vvf_RLXwjzISEyUslqv_O2u8303w',
-        user_agent='YOUR_USER_AGENT'
-    )
-    subreddit_name = subreddit
-    try:
-        # Get the subreddit object
-        subreddit = reddit.subreddit(subreddit_name)
-
-        # Get the number of subscribers
-        subscribers_count = subreddit.subscribers
-
-        return subscribers_count
-
-    except prawcore.exceptions.NotFound:
+    dic = res.json()
+    if 'data' not in dic:
         return 0
-
-
-if __name__ == "__main__":
-    subreddit_name = sys.argv[1]
-    subscribers = number_of_subscribers(subreddit_name)
-#   print(f"The '{subreddit_name}' subreddit has {subscribers} subscribers.")
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']
